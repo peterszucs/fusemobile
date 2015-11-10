e76feee8f414d62a7db491fbf7fbfef17890e4a8
Peter <petermail.88@gmail.com>
2015-11-10 08:04:04 +0100

    Initial Commit

---
+++ app/src/main/java/peter/szucs/fusemobile/MainActivityView.java
@@ -0,0 +1,7 @@
+package peter.szucs.fusemobile;
+
+/**
+ * Created by peter on 2015. 11. 09..
+ */
+public interface MainActivityView {
+}
---
+++ app/src/main/java/peter/szucs/fusemobile/MainPresenter.java
@@ -0,0 +1,16 @@
+package peter.szucs.fusemobile;
+
+/**
+ * Created by peterszucs on 05/11/15.
+ */
+public class MainPresenter {
+
+    MainActivity mainActivity;
+
+    public MainPresenter() {
+
+    }
+
+
+
+}
